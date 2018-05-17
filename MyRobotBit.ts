2de77d363e0af78339040caa3383b09a5b2589a7
RobotStem <38353476+RobotStem@users.noreply.github.com>
2018-05-17 20:54:28 +0700

    Update MyRobotBit.ts

--- MyRobotBit.ts
+++ MyRobotBit.ts
@@ -52,46 +52,46 @@ namespace MyRoboStem {
     }
 
    export enum Servo {
-	//% block="0"
+	//% block="P0"
 	Servo0,
-	//% block="1"
+	//% block="P1"
 	Servo1,
-	//% block="2"
+	//% block="P2"
 	Servo2,
 	//% block="3"
 	Servo3,
-	//% block="4"
+	//% block="P4"
 	Servo4,
-	//% block="10"
+	//% block="P10"
 	Servo10,
-	//% block="5"
+	//% block="P5"
 	Servo5,
-	//% block="6"
+	//% block="P6"
 	Servo6,
-	//% block="7"
+	//% block="P7"
 	Servo7,
-	//% block="8"
+	//% block="P8"
 	Servo8,
-	//% block="9"
+	//% block="P9"
 	Servo9,
-	//% block="11"
+	//% block="P11"
 	Servo11,
-	//% block="12"
+	//% block="P12"
 	Servo12
     }
 
    export enum Servo360 {
 	//% block="0"
 	Servo0,
-	//% block="1"
+	//% block="P1"
 	Servo1,
-	//% block="2"
+	//% block="P2"
 	Servo2,
-	//% block="3"
+	//% block="P3"
 	Servo3,
-	//% block="4"
+	//% block="P4"
 	Servo4,
-	//% block="10"
+	//% block="P10"
 	Servo10
     }
 
